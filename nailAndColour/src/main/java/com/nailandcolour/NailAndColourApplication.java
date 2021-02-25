75cfdbdcbbd050919a220b277fb44ea201b9a308
sheogorath88 <e.slowioczek@gmail.com>
2021-02-25 21:28:51 +0100

    start project

---
+++ nailAndColour/src/main/java/com/nailandcolour/NailAndColourApplication.java
@@ -0,0 +1,15 @@
+package com.nailandcolour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+@SpringBootApplication
+public class NailAndColourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NailAndColourApplication.class, args);
+	}
+	
+
+}
---
+++ nailAndColour/src/test/java/com/nailandcolour/NailAndColourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nailandcolour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NailAndColourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
